3ae00b4e2ec7d7bc5fb35d11e247fae793326d16
HoomanHoo <whqjarn111@gmail.com>
2023-06-27 11:36:11 +0900

    refresh

--- src/main/webapp/misc/orderCheckDetailScript.js
+++
@@ -1,7 +0,0 @@
-let element = document.querySelector("#btnBack");
-element.addEventListener("click", back);
-
-function back(){
-
-	window.location.href = "ordercheck.net";
-}
--- src/main/webapp/misc/orderRequestScript.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(
-	function(){
-		$("input[name=btn]").on("click", function(){
-			let oNum = this.id;
-			window.location.href = "detailrequest.net?onum=" + oNum;
-		});
-	}
-);
-
